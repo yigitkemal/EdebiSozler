971cfb43913d969ee143690aaa44512be485062b
yigitkemal <40325505+yigitkemal@users.noreply.github.com>
2021-09-14 13:45:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/edebisozler/FlowFragment.java
@@ -0,0 +1,26 @@
+package com.example.edebisozler;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class FlowFragment extends Fragment {
+
+
+    public static FlowFragment newInstance(){
+        return new FlowFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_flow, container, false);
+    }
+}
